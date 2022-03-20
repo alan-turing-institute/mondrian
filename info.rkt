2df084b5b11380c7515d0ab575c662e11db9741b
= <jgeddes@turing.ac.uk>
2022-03-20 16:52:46 +0000

    Fix info to include scribblings dependencies

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "mondrian")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "pict-lib" "scribble-math"))
 (define scribblings '(("scribblings/mondrian.scrbl" ())))
-(define pkg-desc "Functional tables with ASCII pretty printing")
+(define pkg-desc "Functional tables with text pretty-printing")
 (define version "0.1")
 (define pkg-authors '(jgeddes@turing.ac.uk))
